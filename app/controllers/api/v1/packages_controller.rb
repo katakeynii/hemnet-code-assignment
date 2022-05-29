fb0032e234f29a5d84ae40e5ec8f8058581bf461
Mohamed Camara Gueye <katakeynii@gmail.com>
2022-05-29 17:27:41 +0000

    adding the controller of packages

---
+++ app/controllers/api/v1/packages_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::PackagesController < ApiController
+
+    def index
+    end
+
+    def create
+    end
+
+    def update
+    end
+end
---
+++ spec/requests/api/v1/packages_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Packages", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
